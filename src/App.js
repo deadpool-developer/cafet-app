2b552eda158522c39bf6ea26217ce1268c0fc9f2
Nikita12112002 <guptaniki488@gmail.com>
2021-10-26 21:36:29 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -7,9 +7,6 @@ function App()
     return (
 
       <>
-      <h1>hello</h1>
-      <Home/>
-      
         <Router>
             <Switch>
                  <Route exact path="/home">
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,25 @@
 import React from "react";
+import "./Home.css";
 
 function Home()
 {
     return(
         <>
-          <h1>Home</h1>
+
+        <h1>GREENCHILLI </h1>
+
+        <button>Sign in</button>
+          <button>Login</button>
+
+        <h3>Order your favourite cuisine</h3>
+          <img src="https://thumbs.dreamstime.com/b/indian-dal-traditional-indian-soup-lentils-indian-dal-food-traditional-indian-soup-lentils-indian-dhal-spicy-curry-bowl-indian-139348337.jpg"
+            width="580" height="580"
+            className="home_img"
+          />
+
+         
+
+          <input type="text" placeholder="Enter your delivery location"/>
         </>
     );
 }
