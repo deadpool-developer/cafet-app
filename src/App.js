f10319b4286c8ba7cb6a46293f3ffb60f453f16c
Aditya Saini <adibhu025@gmail.com>
2021-10-31 23:08:45 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router , Switch , Route } from "react-router-dom";
 import './App.css';
 import Home from "./Home";
+import Login from './Login';
 function App()
 {
     return (
@@ -11,6 +12,9 @@ function App()
             <Switch>
                  <Route exact path="/home">
                     <Home/>
+                </Route>
+                <Route exact path = "/login">
+                    <Login/>
                 </Route>
                 
                 
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import "./Home.css";
+import Typewriter from "typewriter-effect";
 
 function Home() {
   return (
@@ -17,6 +18,22 @@ function Home() {
        <NavLink to="/Login" ><button className="btn_1">Sign in</button> </NavLink>
         <button className="btn_2">Login</button>
       </div>
+      
+      <div className="typewriter">
+       <Typewriter
+       onInit = {(typewriter) => {
+         typewriter
+         .typeString("Hello")
+         .pauseFor(1000)
+         .deleteAll()
+         .typeString("Welcome you")
+         .start();
+         
+       }}
+       />
+      </div>
+
+        
 
       <div style={{ marginLeft: "40px" ,marginTop:"80px" }}>
         <h1>Order your favourite cuisine</h1>
@@ -24,17 +41,21 @@ function Home() {
 
       <img
         src="https://thumbs.dreamstime.com/b/indian-dal-traditional-indian-soup-lentils-indian-dal-food-traditional-indian-soup-lentils-indian-dhal-spicy-curry-bowl-indian-139348337.jpg"
-        width="580"
+        width="830"
         height="650"
         className="home_img"
+        alt =""
       />
 
       <input
         type="text"
         name="delivery"
-        placeholder="Enter your delivery location"
+        placeholder="Enter your choice"
         className="home_input"
       />
+      <button className="find_food_btn">
+        FIND FOOD
+      </button>
 
 </div>
     </>
