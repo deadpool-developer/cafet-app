1804b376e4d74ef3828502c0314ee4245805db6b
Aditya Saini <adibhu025@gmail.com>
2021-11-01 15:12:51 +0530

    commit

--- src/Navbar/navbar.jsx
+++ src/Navbar/navbar.jsx
@@ -1,28 +1,33 @@
 import React from "react";
 import {Container, Nav, NavDropdown, Form, FormControl, Button,Navbar} from 'react-bootstrap';
+import "typeface-cinzel";
+import "typeface-bungee";
+import LoginIcon from '@mui/icons-material/Login';
+
 
 function MainNavbar() {
     return(
-        <Navbar bg="light" expand="lg">
+        <Navbar bg="light" variant="light" expand="xxl">
   <Container fluid>
-    <Navbar.Brand href="#">GreenChilli</Navbar.Brand>
+    <Navbar.Brand href="#" style ={{fontFamily: "Bungee, cursive"}}>GreenChilli</Navbar.Brand>
     <Navbar.Toggle aria-controls="navbarScroll" />
     <Navbar.Collapse id="navbarScroll">
       <Nav
-        className="me-auto my-2 my-lg-0"
+        className="me-auto my-2 my-lg-3 ms-5"
         style={{ maxHeight: '100px' }}
         navbarScroll
       >
-        <Nav.Link href="#action1">Sign In</Nav.Link>
-        <Nav.Link href="#action2">Help</Nav.Link>
-        <NavDropdown title="Link" id="navbarScrollingDropdown">
-          <NavDropdown.Item href="#action3">About Us</NavDropdown.Item>
-          <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
+        <Nav.Link className="ms-3" style= {{fontFamily: "Cinzel, serif"}} href="/Sign_up"><LoginIcon/>Sign In</Nav.Link>
+        <Nav.Link className="ms-3"  style= {{fontFamily: "Cinzel, serif"}} href="#action2">Help</Nav.Link>
+        <NavDropdown className="ms-3"  style= {{fontFamily: "Cinzel, serif"}} title="About Us" id="navbarScrollingDropdown">
+          <NavDropdown.Item href="#action3">Policy</NavDropdown.Item>
+          <NavDropdown.Item href="#action4">Team Members</NavDropdown.Item>
           <NavDropdown.Divider />
           <NavDropdown.Item href="#action5">
-            Something else here
+            KIET GROUPS OF INSTITUTIONS
           </NavDropdown.Item>
         </NavDropdown>
+        <Nav.Link className="ms-3"  style= {{fontFamily: "Cinzel, serif"}} href="/Cart">Cart</Nav.Link>
         {/* <Nav.Link href="#" disabled>
           Link
         </Nav.Link> */}
