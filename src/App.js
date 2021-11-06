3540f5a2e6af80f8093b0d507f7422f809d9667f
Nikita12112002 <guptaniki488@gmail.com>
2021-11-06 23:19:19 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router , Switch , Route } from "react-router-dom";
 import './App.css';
-<<<<<<< HEAD
-import Home from "./Home";
+import HomeMain from "./Home/HomeMain";
 import Login from './Login/Login';
 import SignUp from './SignUp';
-=======
-import HomeMain from "./Home/HomeMain";
-import Login from './server/Login/Login';
-import SignUp from './SignUp';
-import Dashboard from "./server/Dashboard/Dashboard";
-import Preferences from "./server/Preferences/Preferences";
-import useToken from "./useToken";
+
 import MainNavbar from './Navbar/navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
->>>>>>> 106c31afcb1fb610d6bfc62d5f56ef476f3fabcf
 
 function App()
 {
